b1fb2f02a974e8ca687446987bbd386e9c096279
Sherman Chen <shamen.chens@gmail.com>
2014-07-24 23:32:53 +0800

    update color_calc

--- js/color_calc.js
+++ js/color_calc.js
@@ -1,19 +1,17 @@
-'use strict';
-
-(function(exports) {
-  var AnalyzeImage = function() {
-    this.dominantColor;
-    this.patellet;
+$().ready(function(){
+  var src = 'https://fbexternal-a.akamaihd.net/safe_image.php?d=AQADybFjx0Mvbvol&w=470&h=246&url=http%3A%2F%2Fs2.gigacircle.com%2Fmedia%2Fs2_53c2119e512ef.jpg&cfs=1&upscale';
+  var container = document.createElement('div');
+  var image = new Image();
+  image.src = src;
+  image.crossOrigin = '';
+  $(container).append(image);
+  $('body').append(container);
+  image.onload = function() {
+    var container = $(this).parent()[0];
+    var colorThief = new ColorThief();
+    var color = colorThief.getColor(image);
+    container.style.backgroundColor = 'rgb(' + color + ')';
+    container.style.padding = '10px';
+    container.dataset.color = color;
   };
-
-  AnalyzeImage.prototype.calculateColor = function(imageSrc) {
-    var img = new Image();
-    img.crossOrigin = '';
-    img.src = imageSrc;
-    var colorThief = ColorThief();
-    return colorThief.getColor(img);
-  };
-
-  exports.calculateColor = calculateColor;
-
-})(this);
+});
